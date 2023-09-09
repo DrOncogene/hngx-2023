6411ed504cfa578b5711318aa45830e1186b5060
Nasir Abdulrasheed <mypythtesting@gmail.com>
2023-09-09 16:11:46 +0100

    Initial setup, readmes and stage on

---
+++ stage_one/main.py
@@ -0,0 +1,28 @@
+from datetime import datetime
+from flask import Flask, request
+
+
+app = Flask(__name__)
+
+
+@app.get('/api')
+def index() -> dict:
+    """
+    returns a JSON response with the details passed
+    """
+    slack_name = request.args.get("slack_name")
+    track = request.args.get("track")
+
+    return {
+        "slack_name": slack_name,
+        "utc_time": datetime.utcnow().isoformat(),
+        "current_day": datetime.now().strftime("%A"),
+        "track": track,
+        "github_file_url": "",
+        "github_repo_url": "",
+        "status_code": 200
+    }
+
+
+if __name__ == '__main__':
+    app.run(port=8080)
